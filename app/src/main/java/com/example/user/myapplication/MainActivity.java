1ab74eab8409c5112a87faeb923ce6c9e6826ccb
Manoly Vladislav <vladislav.manoly@gmail.com>
2018-09-12 16:14:41 +0300

    Test project

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View v)
+    {
+        TextView t1= (TextView)findViewById(R.id.textView2);
+        t1.setText("Hello world");
+            }
+
+}
